b216288e0b5c8dfab2ce7cfc90f9f134f2796f32
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2025-01-28 22:09:27 +0900

    feat(memo) : localStorage 사용 및 리팩토링

--- memo-project/src/App.js
+++ memo-project/src/App.js
@@ -1,63 +1,72 @@
-import { memo, useState } from "react";
+import { memo, useCallback, useState } from "react";
 import "./App.css";
 import MemoContainer from "./component/MemoContainer";
 import SideBar from "./component/Sidebar";
+import { setItem, getItem } from "./lib/Storage";
+import debounce from "lodash.debounce";
+
+const debouncedSetItem = debounce(setItem, 5000);
 //모든 컴포넌트들의 root 컴포넌트
 //컴포넌트가 변경되면(상태(State,Props)) 화면을 다시 그린다
 function App() {
-  const [memos, setMemos] = useState([
-    {
-      title: "memo 1",
-      content: "this is memo 1",
-      createdAt: 1641225302265, //시간 값
-      updatedAt: 1641225302265, //t시간 값
-    },
-    {
-      title: "memo 2",
-      content: "this is memo 2",
-      createdAt: 1641225309267, //시간 값
-      updatedAt: 1641225309267, //시간 값
-    },
-  ]);
+  const [memos, setMemos] = useState(getItem("memo") || []);
 
   //불변성을 항상 주의하면서 코드를 구현해야한다.
   //객체를 직접적으로 건들지 말고 하나의 배열로 만들어내야함
   const [selectedMemoIndex, setSelectedMemoIndex] = useState(0); // 기본값을 첫 번째 메모로 설정
 
   // 상태를 업데이트하는 함수
-  const setMemo = (newMemo) => {
-    const newMemos = [...memos]; // 배열을 복사
-    newMemos[selectedMemoIndex] = newMemo;
-    setMemos(newMemos); // 상태 업데이트
-  };
+  // memos와selectedMemoIndex가 변경되면 함수를 새로 생성해준다
+  const setMemo = useCallback(
+    (newMemo) => {
+      const newMemos = [...memos]; // 배열을 복사
+      newMemos[selectedMemoIndex] = newMemo;
+      setMemos((memos) => {
+        const newMemos = [...memos];
 
-  const addMemo = () => {
-    const now = new Date().getTime();
-    setMemos([
-      ...memos,
-      {
-        title: "untitled",
-        content: "",
-        createdAt: now,
-        updatedAt: now,
-      },
-    ]);
-    //마지막의 인덱스
+        newMemos[selectedMemoIndex] = newMemo;
+        debouncedSetItem("memo", newMemos);
+        return newMemos;
+      }); // 상태 업데이트
+    },
+    [selectedMemoIndex]
+  );
+
+  const addMemo = useCallback(() => {
+    setMemos((memos) => {
+      const now = new Date().getTime();
+      const newMemos = [
+        ...memos,
+        {
+          title: "untitled",
+          content: "",
+          createdAt: now,
+          updatedAt: now,
+        },
+      ];
+      debouncedSetItem("memo", newMemos);
+      return newMemos;
+    });
     setSelectedMemoIndex(memos.length);
-  };
+  }, [memos]);
 
   //불변성 주의
-  const deleteMemo = (index) => {
-    const newMemos = [...memos];
+  const deleteMemo = useCallback(
+    (index) => {
+      setMemos((memos) => {
+        const newMemos = [...memos];
+        newMemos.splice(index, 1);
+        debouncedSetItem("memo", newMemos);
+        return newMemos;
+      });
 
-    newMemos.splice(index, 1);
-
-    setMemos(newMemos);
-    if (index === selectedMemoIndex) {
-      // If the deleted memo was the selected one, select the first memo
-      setSelectedMemoIndex(0);
-    }
-  };
+      if (index === selectedMemoIndex) {
+        // If the deleted memo was the selected one, select the first memo
+        setSelectedMemoIndex(0);
+      }
+    },
+    [selectedMemoIndex]
+  );
 
   return (
     <div className="App">
